57189822424ced5f0eb85c7405bfa8ae2eeceb5b
Dashboard <elec0807@gmail.com>
2024-11-07 09:40:37 +0900

    test

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const PORT = 8000;
 
-const express = require("express"); // express 모듈을 가져온다
-const cors = require("cors"); // cors 모듈을 가져온다
+const express = require("express");
+const cors = require("cors");
 
-const app = express(); // express 모듈의 기능을 사용하기 위해 app 변수에 객체 할당
-app.use(cors()); // http, https 프로토콜을 사용하는 서버 간의 통신을 허용한다
-app.use(express.json()); // express 모듈의 json() 매서드를 사용한다
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello world Node Test"); // 브라우저에 표시
+  res.send("Hello World Tasks App Node Test Root");
 });
 
-app.use(require("./router/getTasksCtrl"));
+app.use(require("./routes/getRoutes"));
 
-app.listen(PORT, () => console.log(`Sever is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
