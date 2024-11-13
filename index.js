1294a14d8135eab4a5ac305c32b693de28ff01f5
Dashboard <elec0807@gmail.com>
2024-11-13 15:21:20 +0900

    test 2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("node deploy test 1");
+  res.send("node deploy test 2");
 });
 
 app.use(require("./routes/getRoutes"));
